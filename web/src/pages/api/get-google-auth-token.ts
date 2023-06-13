55a4e4a23466557ef3bd2ce15401e3c48155c141
claustra01 <shinya430.g@gmail.com>
2023-06-14 08:41:36 +0900

    :adhesive_bandage: change callback uri path

--- web/src/pages/api/get-google-auth-token.ts
+++ web/src/pages/api/get-google-auth-token.ts
@@ -7,7 +7,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const oauth2Client = new google.auth.OAuth2({
     clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
     clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-    redirectUri: process.env.ORIGIN_URI + '/google-callback'
+    redirectUri: process.env.ORIGIN_URI + '/callback'
   })
  
   const response = await oauth2Client.getToken(authorizationCode)
