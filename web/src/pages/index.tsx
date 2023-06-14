6de0aa7a44ed9511bc9ad4a7c93ceb1c87e97171
claustra01 <shinya430.g@gmail.com>
2023-06-14 13:18:53 +0900

    :construction: save line id in cookie

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,19 +2,30 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import axios from 'axios'
 import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { useCookies } from 'react-cookie'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
+  const router = useRouter()
+  const [cookie, setCookie, removeCookie] = useCookies(['id'])
+  
   useEffect(() => {
+
+    const lineId = router.query.id
+    if (!lineId) return
+    setCookie('id', lineId)
+
     const loginWithGoogle = async () => {
       const response = await axios.get('/api/generate-google-oauth-url')
       const { authorizeUrl } = response.data
       location.href = authorizeUrl
     }
     loginWithGoogle()
-  })
+
+  }, [router.query.id])
 
   return (
     <>
