5dabe5c64829d2e0143fd037e6dc0b1d525b9836
claustra01 <shinya430.g@gmail.com>
2023-06-14 10:57:50 +0900

    :zap: open line app after login

--- web/src/pages/result.tsx
+++ web/src/pages/result.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 
 export default function Result() {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace('line://')
+  })
  
   return (
     <>
