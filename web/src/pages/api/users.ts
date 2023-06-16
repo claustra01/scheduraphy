50be0c0967b4c2c89b72b35d21f410749a7ab91a
claustra01 <shinya430.g@gmail.com>
2023-06-16 09:57:28 +0900

    :sparkles: get user

--- web/src/pages/api/users.ts
+++ web/src/pages/api/users.ts
@@ -1,10 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { User } from './db/models';
 import { NOW, UUIDV4 } from 'sequelize';
+import { type } from 'os';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
   switch (req.method) {
+
+    case 'GET':
+      try {
+        const lineId = req.query.lineId
+        const user = await User.findOne({
+          where: {
+            line_id: lineId
+          }
+        })
+        if (user != null) {
+          res.status(200).json(user.toJSON)
+        } else {
+          res.status(503).end()     
+        }
+      } catch(error) {
+        console.error(error)   
+        res.status(503).end()     
+      }
+      break
 
     case 'POST':
       try {
@@ -13,8 +33,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           line_id: lineId,
           refresh_token: refreshToken,
         });
-        res.status(200).json(user.toJSON())
-      } catch (error) {
+        if (user != null) {
+          res.status(200).json(user.toJSON)
+        } else {
+          res.status(503).end()     
+        }
+      } catch(error) {
         console.error(error)   
         res.status(503).end()     
       }
