6d20db6c02bd747bbb8ece2f79ad8effbe8f7f57
claustra01 <shinya430.g@gmail.com>
2023-06-17 19:02:46 +0900

    :sparkles: extract char

---
+++ bot/util/extract_char.go
@@ -0,0 +1,62 @@
+package util
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"os"
+)
+
+func ExtractChar(file *os.File) {
+
+	key := os.Getenv("AZURE_COMPUTER_VISION_KEY")
+	endpoint := os.Getenv("AZURE_COMPUTER_VISION_ENDPOINT")
+
+	// 画像の読み込み
+	var buf bytes.Buffer
+	w := multipart.NewWriter(&buf)
+	fw, _ := w.CreateFormFile("data", "image")
+	io.Copy(fw, file)
+	file.Close()
+	w.Close()
+
+	// REST APIの実行
+	uri := endpoint + "/vision/v2.1/ocr?language=unk&detectOrientation=true"
+	req, _ := http.NewRequest("POST", uri, &buf)
+	req.Header.Add("Content-Type", w.FormDataContentType())
+	req.Header.Add("Ocp-Apim-Subscription-Key", key)
+	client := &http.Client{}
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+
+	// JSONに展開
+	var data interface{}
+	dataStr, _ := ioutil.ReadAll(resp.Body)
+	err := json.Unmarshal([]byte(dataStr), &data)
+	if err != nil {
+		fmt.Println("JSON parsing error:", err)
+		return
+	}
+
+	// 文字列に整形
+	extractedString := ""
+	regions := data.(map[string]interface{})["regions"].([]interface{})
+	for _, region := range regions {
+		lines := region.(map[string]interface{})["lines"].([]interface{})
+		for _, line := range lines {
+			words := line.(map[string]interface{})["words"].([]interface{})
+			for _, word := range words {
+				text := word.(map[string]interface{})["text"].(string)
+				extractedString += text
+			}
+			extractedString += "\n"
+		}
+	}
+
+	fmt.Println(extractedString)
+
+}
